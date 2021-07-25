d7cd4a25510f106c0fd5f111f24f59d3b97cf4ab
Vipin <Vipin@localhost>
2021-07-24 18:15:48 -0700

    First test

---
+++ src/main/java/com/vipin/utils/Utils.java
@@ -0,0 +1,15 @@
+package com.vipin.utils;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vipin
+ */
+public class Utils {
+    
+}
